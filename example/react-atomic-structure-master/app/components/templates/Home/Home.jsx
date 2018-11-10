2e4fcb8d084d4d1fe358c6e4965a14ac738983bf
Rulox <marreror91@gmail.com>
2018-11-10 22:06:01 +0000

    Use gdrive slides instead of deckjs

--- example/react-atomic-structure-master/app/components/templates/Home/Home.jsx
+++ example/react-atomic-structure-master/app/components/templates/Home/Home.jsx
@@ -12,16 +12,19 @@ require('./_style.scss');
 
 const posts = [
   {
+    id: 1,
     title: 'I painted this',
     text: 'Congue eu consequat ac felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla',
     image: image1,
   },
   {
+    id: 2,
     title: 'OMG! I\'m the new face for this awesome Brand!',
     text: 'Congue eu consequat ac felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla',
     image: image2,
   },
   {
+    id: 3,
     title: 'Sometimes, I like to sit on Windows...',
     text: 'Congue eu consequat ac felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla',
     image: image3,
@@ -31,9 +34,9 @@ const posts = [
 const Home = props => (
   <div>
     <Title text="Photogram" />
-    {posts.map((post, i) => {
+    {posts.map((post) => {
       return (
-        <Post key={i} image={post.image} title={post.title} text={post.text} />
+        <Post key={post.id} image={post.image} title={post.title} text={post.text} />
       );
     })}
   </div>
